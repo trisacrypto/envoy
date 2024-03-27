d21325eaaf79beb1af0c5a393901ff2fa3f1592f
Danielle <danielle@rotational.io>
2024-03-27 13:12:56 -0400

    Display secure envelope preview modal to users (#31)

--- pkg/web/static/js/envelope.js
+++ pkg/web/static/js/envelope.js
@@ -1,22 +1,30 @@
-const openModal = document.getElementById('open-envelope-modal')
-const closeModal = document.getElementById('close-envelope-modal')
-const previewModal = document.getElementById('preview-envelope-modal')
-const modalOverlay = document.getElementById('preview-envelope-overlay')
+const previewEnvelopeBttn = document.getElementById('preview-envelope-bttn')
+const secureEnvelopeForm = document.getElementById('secure-envelope-form')
 
-openModal.addEventListener('click', () => {
-  previewModal.classList.remove('hidden');
-  modalOverlay.classList.remove('hidden');
+// Get form values and display in preview modal.
+previewEnvelopeBttn?.addEventListener('click', () => {
+  const formData = new FormData(secureEnvelopeForm);
+  const envelopeData = Object.fromEntries(formData);
 
-  // Prevent users from scrolling when the modal is open.
-  document.body.style.overflow = 'hidden';
+  // Originator Info Form Values
+  document.getElementById('orig-name').textContent = envelopeData.originator_first_name + ' ' + envelopeData.originator_last_name
+  document.getElementById('orig-internal-acct').textContent = envelopeData.customer_identifier
+  document.getElementById('orig-addr-one').textContent = envelopeData.address_one
+
+  if (envelopeData.address_two) {
+    document.getElementById('orig-addr-two').textContent = envelopeData.address_two
+  }
+
+  document.getElementById('orig-addr-three').textContent = envelopeData.city + ' ' + envelopeData.region + ' ' + envelopeData.postal_code
+  document.getElementById('orig-country').textContent = envelopeData.country
+
+  // Beneficiary Info Form Values
+  document.getElementById('benf-name').textContent = envelopeData.beneficiary_first_name + ' ' + envelopeData.beneficiary_last_name
+  document.getElementById('benf-vasp-name').textContent = envelopeData.beneficiary_vasp
+  document.getElementById('benf-wallet-addr').textContent = envelopeData.wallet_address
+
+  // Virtual Asset Form Values
+  document.getElementById('asset-type').textContent = envelopeData.asset_type
+  document.getElementById('transfer-amt').textContent = envelopeData.amount
+  document.getElementById('transfer-tag').textContent = envelopeData.tag
 });
-
-if (closeModal) {
-  closeModal.addEventListener('click', () => {
-    previewModal.classList.add('hidden');
-    modalOverlay.classList.add('hidden');
-
-    // Reset scrolling behavior when the modal is closed.
-    document.body.style.overflow = 'auto';
-  })
-};
