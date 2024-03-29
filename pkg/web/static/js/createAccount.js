435b2b7d6023f8ef1383fd2988094d15d8a34602
daniellemaxwell <danielle@rotational.io>
2024-03-29 08:51:19 -0400

    Add wallet count number to input names, remove scrollbars when actions menu is displayed

--- pkg/web/static/js/createAccount.js
+++ pkg/web/static/js/createAccount.js
@@ -7,12 +7,12 @@ addWalletBttn?.addEventListener('click', () => {
   <div class="grid gap-6 my-4 md:grid-cols-2">
     <div>
       <label for="crypto_address" class="label-style">Wallet Address ${walletCount}</label>
-      <input type="text" id="crypto_address" name="crypto_address" class="input-style" />
+      <input type="text" id="crypto_address" name="crypto_address_${walletCount}" class="input-style" />
     </div>
     <div>
       <label for="network" class="label-style">Network</label>
       <div class="flex items-center gap-x-1">
-        <input type="text" id="network" name="network" class="input-style" />
+        <input type="text" id="network" name="network_${walletCount}" class="input-style" />
         <button type="button" onclick="this.parentNode.parentNode.parentNode.remove()">
           <i class="fa-solid fa-trash"><span class="sr-only">Delete wallet</span></i>
         </button>
