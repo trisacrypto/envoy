d7165a4b79f6a09bdafcd28e9cd21afa283f3bd7
Benjamin Bengfort <benjamin@bengfort.com>
2024-04-04 18:05:09 -0500

    deal with db scanner and valuers

--- pkg/store/models/transaction.go
+++ pkg/store/models/transaction.go
@@ -2,11 +2,21 @@ package models
 
 import (
 	"self-hosted-node/pkg/store/errors"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/oklog/ulid/v2"
 )
 
 type Transaction struct {
-	Model
-	envelopes []*SecureEnvelope // Associated secure envelopes
+	ID             uuid.UUID         // Transaction IDs are UUIDs not ULIDs per the TRISA spec, this is also used for the envelope ID
+	Source         string            // Either "local" meaning the transaction was created by the user, or "remote" meaning it is an incoming message
+	Status         string            // Can be "draft", "pending", "action required", "completed", "archived"
+	Counterparty   string            // The name of the counterparty in the transaction
+	CounterpartyID ulid.ULID         // A reference to the counterparty in the database, if any
+	Created        time.Time         // Timestamp the transaction was created
+	Modified       time.Time         // Timestamp the transaction was last modified, including when a new secure envelope was received
+	envelopes      []*SecureEnvelope // Associated secure envelopes
 }
 
 type SecureEnvelope struct {
