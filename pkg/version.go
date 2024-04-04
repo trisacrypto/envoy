bc1bbfd20147ed93df746e91dc26bc6c6c692a2c
Benjamin Bengfort <benjamin@bengfort.com>
2024-04-04 13:20:14 -0500

    version 0.4.0 bump

--- pkg/version.go
+++ pkg/version.go
@@ -5,10 +5,10 @@ import "fmt"
 // Version component constants for the current build.
 const (
 	VersionMajor         = 0
-	VersionMinor         = 3
+	VersionMinor         = 4
 	VersionPatch         = 0
 	VersionReleaseLevel  = "alpha"
-	VersionReleaseNumber = 3
+	VersionReleaseNumber = 4
 )
 
 // Set the GitVersion via -ldflags="-X 'github.com/trisacrypto/self-hosted-node/pkg.GitVersion=$(git rev-parse --short HEAD)'"
