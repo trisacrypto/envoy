445e680b4cb75937dff4e4281eef81b821331ae0
daniellemaxwell <danielle@rotational.io>
2024-04-04 08:42:24 -0400

    Close register acct modal on successful submission

--- pkg/web/static/js/customerAccounts.js
+++ pkg/web/static/js/customerAccounts.js
@@ -1,6 +1,6 @@
 const addWalletBttn = document.getElementById('add-wallet-bttn')
 const extractWalletRE = /(crypto_address|network)_(\d+)/g;
-
+const newAcctForm = document.getElementById('new-acct-form')
 
 document.body.addEventListener("htmx:configRequest", (e) => {
   // Check if this is a POST request for the accounts form.
@@ -57,3 +57,13 @@ addWalletBttn?.addEventListener('click', () => {
   </div>
   `)
 })
+
+
+document.body.addEventListener('htmx:afterRequest', (e) => {
+  // Check if the request to register a new customer account was successful.
+  if (e.detail.elt.id === newAcctForm && e.detail.requestConfig.verb === 'post' && e.detail.successful) {
+    // Close the modal and reset the form values.
+    document.getElementById('new_acct_modal').close()
+    newAcctForm.reset()
+  }
+});
