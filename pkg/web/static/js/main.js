bc621694b9bcbdb96120b2b716ebbf4e2f6d8c20
Danielle <danielle@rotational.io>
2024-04-18 17:24:13 -0400

    Ensure welcome message remains hidden after close (#70)

--- pkg/web/static/js/main.js
+++ pkg/web/static/js/main.js
@@ -1,3 +1,26 @@
 document.body.addEventListener('htmx:configRequest', function(e) {
   e.detail.headers['Accept'] = 'text/html'
 });
+
+const hideWelcomeMsg = 'hideWelcomeMsg'
+const welcomeMsg = document.getElementById('welcome-msg')
+const closeMsg = document.getElementById('close-welcome-msg')
+const logoutBttn = document.getElementById('logout-bttn')
+
+// Hide welcome message during a session if user clicks the close button.
+if (closeMsg) {
+  closeMsg.addEventListener('click', () => {
+    welcomeMsg.classList.add('hidden')
+    localStorage.setItem(hideWelcomeMsg, 'true')
+  })
+}
+
+if (localStorage.getItem(hideWelcomeMsg) === 'true') {
+  welcomeMsg?.classList.add('hidden')
+}
+
+if (logoutBttn) {
+  logoutBttn.addEventListener('click', () => {
+    localStorage.removeItem(hideWelcomeMsg)
+  })
+}
