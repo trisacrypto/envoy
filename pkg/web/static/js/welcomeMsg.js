e28197b0f171abe863883180ec229cca96dc17fd
daniellemaxwell <danielle@rotational.io>
2024-04-22 16:11:29 -0400

    Revert changes to welcomeMsg script

--- pkg/web/static/js/welcomeMsg.js
+++ pkg/web/static/js/welcomeMsg.js
@@ -1,1 +1,14 @@
+const closeMsg = document.getElementById('close-welcome-msg')
+const welcomeMsg = document.getElementById('welcome-msg')
+
+if (closeMsg) {
+  closeMsg.addEventListener('click', () => {
+    welcomeMsg.classList.add('hidden')
+    localStorage.setItem('hideWelcomeMsg', 'true')
+  })
+}
+
+if (localStorage.getItem('hideWelcomeMsg') === 'true') {
+  welcomeMsg.classList.add('hidden')
+}
 
