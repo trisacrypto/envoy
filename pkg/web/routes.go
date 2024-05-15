53172846a84eb99c04a5fdf8d5f87b0213bf7b85
daniellemaxwell <danielle@rotational.io>
2024-05-15 15:15:23 -0400

    Create page to edit counterparty details

--- pkg/web/routes.go
+++ pkg/web/routes.go
@@ -93,6 +93,7 @@ func (s *Server) setupRoutes() (err error) {
 	s.router.GET("/transactions/:id/accept", authenticate, s.TransactionsAcceptPreview)
 	s.router.GET("/accounts", authenticate, s.Accounts)
 	s.router.GET("/counterparty", authenticate, s.CounterpartyVasps)
+	s.router.GET("/counterparty/:id/edit", authenticate, s.CounterpartyUpdate)
 	s.router.GET("/send-envelope", authenticate, s.SendEnvelopeForm)
 	s.router.GET("/utilities/travel-address", authenticate, s.TravelAddressUtility)
 
