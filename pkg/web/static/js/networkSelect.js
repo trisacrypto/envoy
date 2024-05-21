66a29543a2c22009bb802429944c90657eb8dc45
Danielle <danielle@rotational.io>
2024-05-21 16:17:24 -0400

    Display transaction details in form (#100)

--- pkg/web/static/js/networkSelect.js
+++ pkg/web/static/js/networkSelect.js
@@ -1,3 +1,5 @@
+import { networksArray } from "./constants.js";
+
 const networkContentDiv = document.getElementById('network-content')
 // Display a searchable dropdown for networks.
 const networkSelect = new SlimSelect({
@@ -7,33 +9,5 @@ const networkSelect = new SlimSelect({
   },
 });
 
-const networks = {
-  "BTC": "Bitcoin",
-  "ETH": "Ethereum",
-  "AGM": "Argoneum",
-  "BCH": "Bitcoin Cash",
-  "BTG": "Bitcoin Gold",
-  "XBC": "Bitcoinplus",
-  "BTX": "BitCore",
-  "CHC": "Chaincoin",
-  "DASH": "Dash",
-  "DOGEC": "DogeCash",
-  "DOGE": "Dogecoin",
-  "FTC": "Feathercoin",
-  "GRS": "Groestlcoin",
-  "KOTO": "Koto",
-  "LBTC": "Liquid",
-  "LTC": "Litecoin",
-  "MONA": "Monacoin",
-  "POLIS": "Polis",
-  "TRC": "Terracoin",
-  "UFO": "UFO",
-  "XVG": "Verge Currency",
-  "VIA": "Viacoin",
-  "ZCL": "Zclassic",
-  "XZC": "ZCoin"
-};
-
-const networksArray = Object.entries(networks).map(([value, text]) => ({ text, value }));
 networksArray.unshift({ 'placeholder': true, 'text': 'Select a network', 'value': '' });
 networkSelect.setData(networksArray);
