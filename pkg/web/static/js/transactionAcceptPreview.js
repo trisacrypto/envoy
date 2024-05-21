a57a72997e12fbb9cb7a57992f89be09e19b3b21
daniellemaxwell <danielle@rotational.io>
2024-05-21 14:16:00 -0400

    Add comments to script, rebuild output.css file

--- pkg/web/static/js/transactionAcceptPreview.js
+++ pkg/web/static/js/transactionAcceptPreview.js
@@ -8,10 +8,9 @@ document.body.addEventListener('htmx:afterSettle', () => {
     }
   })
 
+  // Set the list of options in the network dropdown and display the value selected by the requester.
   const networkEl = document.getElementById('selected-network');
   const networkValue = networkEl.value;
-
-
   transactionNetwork.setData(networksArray);
   transactionNetwork.setSelected(networkValue);
 })
