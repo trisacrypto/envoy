925b3dd05202253b10e7aa6961228fb822c555ce
Danielle <danielle@rotational.io>
2024-06-05 16:36:58 -0400

    Replace network and country inputs with select elements (#121)

--- pkg/web/static/js/transactionAcceptPreview.js
+++ pkg/web/static/js/transactionAcceptPreview.js
@@ -1,8 +1,9 @@
 import { networksArray } from "./constants.js";
 
 document.body.addEventListener('htmx:afterSettle', () => {
+  // Initialize a SlimSelect dropdown for the transaction network.
   const transactionNetwork = new SlimSelect({
-    select: "#transaction-networks",
+    select: "#network",
     settings: {
       contentLocation: document.getElementById('network-content')
     }
@@ -13,4 +14,28 @@ document.body.addEventListener('htmx:afterSettle', () => {
   const networkValue = networkEl.value;
   transactionNetwork.setData(networksArray);
   transactionNetwork.setSelected(networkValue);
+
+  // Initialize a SlimSelect dropdown for each country selection in the form.
+  const countries = document.querySelectorAll('.countries')
+  countries.forEach((country) => {
+    new SlimSelect({
+      select: country,
+    })
+
+    // Get each country value selected by the requester from the hidden input field.
+    const countryID = country.id
+    const selectedCountry = document.querySelector(`.${countryID}`)
+
+    if (selectedCountry) {
+      const countryValue = selectedCountry.value
+      setSlimData(country, countryValue)
+    }
+
+  })
 })
+
+// Set the country options and selected value in a SlimSelect dropdown for each country selection.
+function setSlimData(el, value) {
+  el.slim.setData(countriesArray)
+  el.slim.setSelected(value)
+}
