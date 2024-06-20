62d636df215f015dcbdebda0f61957559786439c
daniellemaxwell <danielle@rotational.io>
2024-06-20 19:46:20 -0400

    Use class to add slim select dropdowns for additional networks

--- pkg/web/static/js/customerAccounts.js
+++ pkg/web/static/js/customerAccounts.js
@@ -42,9 +42,13 @@ document.body.addEventListener("htmx:configRequest", (e) => {
   }
 });
 
+// Count the number of wallet addresses to ensure a unique number as crypto wallets or added and deleted.
+// Start the counter at 1 to avoid a conflict with the first crypto wallet.
+let walletIdCounter = 1;
+
 // Add a new wallet address and network field to the new customer account form modal on click.
 addWalletBttn?.addEventListener('click', () => {
-  const walletCount = walletDiv?.children.length * 1
+  const walletCount = walletIdCounter;
   walletDiv?.insertAdjacentHTML('beforeend', `
   <div class="grid gap-6 my-4 md:grid-cols-2 crypto-wallets">
     <div>
@@ -54,29 +58,37 @@ addWalletBttn?.addEventListener('click', () => {
     <div>
       <label for="network_${walletCount}" class="label-style">Network</label>
       <div class="flex items-center gap-x-1">
-        <select id="network_${walletCount}" name="network_${walletCount}"></select>
+        <select id="network_${walletCount}" name="network_${walletCount}" class="acct-networks"></select>
         <button type="button" onclick="this.parentNode.parentNode.parentNode.remove()" class="tooltip tooltip-left" data-tip="Delete wallet">
           <i class="fa-solid fa-trash text-xs"><span class="sr-only">Delete wallet</span></i>
         </button>
       </div>
     </div>
   </div>
-  `)
+  `);
 
-  // TODO: Does a new div need to be created for each network select?
-  // Create a div to ensure content appears in the modal and not the document body.
-  newAcctModal.appendChild(document.createElement('div')).id = `network_list_${walletCount}`
+  // Increment the wallet counter for the next wallet address.
+  walletIdCounter++;
 
-  // Initialize network select for each new wallet.
-  const additionalNetworkSelect = new SlimSelect({
-    select: `#network_${walletCount}`,
-    settings: {
-      contentLocation: document.getElementById(`network_list_${walletCount}`),
-    },
+  // Create a searchable select dropdown for the network when a new wallet is added.
+  const acctNetworks = document.querySelectorAll('.acct-networks')
+  // Initialize SlimSelect for each crypto wallet network.
+  acctNetworks.forEach((network) => {
+    new SlimSelect({
+      select: network,
+      settings: {
+        contentLocation: document.getElementById('new_acct_modal')
+      }
+    });
+
+    // Get the selected network value for each additional wallet and set it as the selected value
+    // to ensure the dropdown does not reset the value when a new wallet is added.
+    const selectedValue = network?.value
+
+    // Add network options to each additional wallet.
+    network?.slim?.setData(networksArray);
+    network?.slim?.setSelected(selectedValue);
   })
-
-  // Add network options to additional wallet.
-  additionalNetworkSelect.setData(networksArray);
 })
 
 // Close the new customer account modal and reset the form values on success.
@@ -113,11 +125,11 @@ document.body.addEventListener('htmx:afterSettle', (e) => {
       });
 
       // Get each network value selected by the requester from the hidden input field.
-      const networkID = network.id;
+      const networkID = network?.id;
       const selectedNetwork = document.querySelector(`.${networkID}`);
 
       if (selectedNetwork) {
-        const networkValue = selectedNetwork.value;
+        const networkValue = selectedNetwork?.value;
         setNetworkData(network, networkValue);
       };
     });
@@ -127,6 +139,6 @@ document.body.addEventListener('htmx:afterSettle', (e) => {
 // Set the network options and selected value in a SlimSelect dropdown for each network.
 function setNetworkData(el, value) {
   // Add network options to the select element.
-  el.slim.setData(networksArray);
-  el.slim.setSelected(value);
+  el?.slim?.setData(networksArray);
+  el?.slim?.setSelected(value);
 }
