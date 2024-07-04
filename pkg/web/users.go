3193eea512c5e73fab3cdc7f54086302c1c323a1
Benjamin Bengfort <benjamin@bengfort.com>
2024-07-04 15:47:35 -0500

    implement api handlers

--- pkg/web/users.go
+++ pkg/web/users.go
@@ -1,13 +1,253 @@
 package web
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+	"net/http"
 
-func (s *Server) ListUsers(c *gin.Context) {}
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/oklog/ulid/v2"
+	dberr "github.com/trisacrypto/envoy/pkg/store/errors"
+	"github.com/trisacrypto/envoy/pkg/store/models"
+	"github.com/trisacrypto/envoy/pkg/ulids"
+	"github.com/trisacrypto/envoy/pkg/web/api/v1"
+)
 
-func (s *Server) CreateUser(c *gin.Context) {}
+func (s *Server) ListUsers(c *gin.Context) {
+	var (
+		err   error
+		in    *api.PageQuery
+		query *models.PageInfo
+		page  *models.UserPage
+		out   *api.UserList
+	)
 
-func (s *Server) UserDetail(c *gin.Context) {}
+	// Parse the URL parameters from the input request
+	in = &api.PageQuery{}
+	if err = c.BindQuery(in); err != nil {
+		c.Error(err)
+		c.JSON(http.StatusBadRequest, api.Error("could not parse page query request"))
+		return
+	}
 
-func (s *Server) UpdateUser(c *gin.Context) {}
+	// TODO: implement better pagination mechanism (with pagination tokens)
 
-func (s *Server) DeleteUser(c *gin.Context) {}
+	// Fetch the list of users from the database
+	if page, err = s.store.ListUsers(c.Request.Context(), query); err != nil {
+		c.Error(err)
+		c.JSON(http.StatusInternalServerError, api.Error("could not process user list request"))
+		return
+	}
+
+	// convert the users page into a users list object
+	if out, err = api.NewUserList(page); err != nil {
+		c.Error(err)
+		c.JSON(http.StatusInternalServerError, api.Error("could not process user list request"))
+		return
+	}
+
+	// Content negotiation
+	c.Negotiate(http.StatusOK, gin.Negotiate{
+		Offered:  []string{binding.MIMEJSON, binding.MIMEHTML},
+		Data:     out,
+		HTMLName: "user_list.html",
+	})
+}
+
+func (s *Server) CreateUser(c *gin.Context) {
+	var (
+		err  error
+		in   *api.User
+		user *models.User
+		out  *api.User
+	)
+
+	// Parse the model from the POST request
+	in = &api.User{}
+	if err = c.BindJSON(in); err != nil {
+		c.Error(err)
+		c.JSON(http.StatusBadRequest, api.Error("could not parse user data"))
+		return
+	}
+
+	if !ulids.IsZero(in.ID) {
+		c.JSON(http.StatusBadRequest, api.Error("cannot specify an id when creating a user"))
+		return
+	}
+
+	if err = in.Validate(); err != nil {
+		c.JSON(http.StatusBadRequest, api.Error(err))
+		return
+	}
+
+	// TODO: deal with role and password for the new user
+
+	// Convert the API serializer into a dtabase model
+	if user, err = in.Model(); err != nil {
+		c.Error(err)
+		c.JSON(http.StatusBadRequest, api.Error(err))
+		return
+	}
+
+	// Create the model in the database (which will update the pointer)
+	if err = s.store.CreateUser(c.Request.Context(), user); err != nil {
+		// TODO: handle other error types that would return a 400
+		c.Error(err)
+		c.JSON(http.StatusInternalServerError, api.Error(err))
+		return
+	}
+
+	// Convert the model back to an API response
+	if out, err = api.NewUser(user); err != nil {
+		c.Error(err)
+		c.JSON(http.StatusInternalServerError, api.Error("could not process create user request"))
+		return
+	}
+
+	c.Negotiate(http.StatusOK, gin.Negotiate{
+		Offered:  []string{binding.MIMEJSON, binding.MIMEHTML},
+		Data:     out,
+		HTMLName: "user_create.html",
+	})
+}
+
+func (s *Server) UserDetail(c *gin.Context) {
+	var (
+		err    error
+		userID ulid.ULID
+		user   *models.User
+		out    *api.User
+	)
+
+	// Parse the userID from the URL
+	if userID, err = ulid.Parse(c.Param("id")); err != nil {
+		c.JSON(http.StatusNotFound, api.Error("user not found"))
+		return
+	}
+
+	// Fetch the model from the database
+	if user, err = s.store.RetrieveUser(c.Request.Context(), userID); err != nil {
+		if errors.Is(err, dberr.ErrNotFound) {
+			c.JSON(http.StatusNotFound, api.Error("user not found"))
+			return
+		}
+
+		c.Error(err)
+		c.JSON(http.StatusInternalServerError, api.Error("unable to fetch specified user"))
+		return
+	}
+
+	if out, err = api.NewUser(user); err != nil {
+		c.Error(err)
+		c.JSON(http.StatusInternalServerError, api.Error("unable to fetch specified user"))
+		return
+	}
+
+	c.Negotiate(http.StatusOK, gin.Negotiate{
+		Offered:  []string{binding.MIMEJSON, binding.MIMEHTML},
+		Data:     out,
+		HTMLName: "user_detail.html",
+	})
+}
+
+func (s *Server) UpdateUser(c *gin.Context) {
+	var (
+		err    error
+		userID ulid.ULID
+		user   *models.User
+		in     *api.User
+		out    *api.User
+	)
+
+	// Parse the userID from the URL
+	if userID, err = ulid.Parse(c.Param("id")); err != nil {
+		c.JSON(http.StatusNotFound, api.Error("user not found"))
+		return
+	}
+
+	// Parse the user data for the update request
+	in = &api.User{}
+	if err = c.BindJSON(in); err != nil {
+		c.Error(err)
+		c.JSON(http.StatusBadRequest, api.Error("could not parse user data"))
+		return
+	}
+
+	// Sanity check
+	if err = ulids.CheckIDMatch(in.ID, userID); err != nil {
+		c.Error(err)
+		c.JSON(http.StatusBadRequest, api.Error(err))
+		return
+	}
+
+	// Validation
+	if err = in.Validate(); err != nil {
+		c.Error(err)
+		c.JSON(http.StatusBadRequest, api.Error(err))
+		return
+	}
+
+	// Extract the model
+	if user, err = in.Model(); err != nil {
+		c.Error(err)
+		c.JSON(http.StatusBadRequest, api.Error(err))
+		return
+	}
+
+	if err = s.store.UpdateUser(c.Request.Context(), user); err != nil {
+		if errors.Is(err, dberr.ErrNotFound) {
+			c.JSON(http.StatusNotFound, api.Error("user not found"))
+			return
+		}
+
+		// TODO: handle other types of database errors and constraints
+		c.Error(err)
+		c.JSON(http.StatusInternalServerError, api.Error(err))
+		return
+	}
+
+	// Convert model back to an API response
+	if out, err = api.NewUser(user); err != nil {
+		c.Error(err)
+		c.JSON(http.StatusInternalServerError, api.Error(err))
+		return
+	}
+
+	c.Negotiate(http.StatusOK, gin.Negotiate{
+		Offered:  []string{binding.MIMEJSON, binding.MIMEHTML},
+		Data:     out,
+		HTMLName: "user_update.html",
+	})
+}
+
+func (s *Server) DeleteUser(c *gin.Context) {
+	var (
+		err    error
+		userID ulid.ULID
+	)
+
+	// Parse the userID from the URL
+	if userID, err = ulid.Parse(c.Param("id")); err != nil {
+		c.JSON(http.StatusNotFound, api.Error("user not found"))
+		return
+	}
+
+	// Delete the user from the database
+	if err = s.store.DeleteUser(c.Request.Context(), userID); err != nil {
+		if errors.Is(err, dberr.ErrNotFound) {
+			c.JSON(http.StatusNotFound, api.Error("user not found"))
+			return
+		}
+
+		c.Error(err)
+		c.JSON(http.StatusInternalServerError, api.Error(err))
+		return
+	}
+
+	c.Negotiate(http.StatusOK, gin.Negotiate{
+		Offered:  []string{binding.MIMEJSON, binding.MIMEHTML},
+		HTMLData: gin.H{"UserID": userID},
+		JSONData: api.Reply{Success: true},
+		HTMLName: "user_delete.html",
+	})
+}
