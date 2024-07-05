1a95742dd53e80cd5d62f6d98f760512e08f3fdf
daniellemaxwell <danielle@rotational.io>
2024-07-05 18:24:07 -0400

    Reset form when change pwd modal is closed

--- pkg/web/static/js/userProfile.js
+++ pkg/web/static/js/userProfile.js
@@ -1,17 +1,26 @@
+const changePwdModal = document.getElementById('change_pwd_modal');
+const changePwdForm = document.getElementById('change-pwd-form')
+const closePwdModalBtn = document.getElementById('close-pwd-mdl-btn');
+
+// Reset the form when change password modal is closed in case user entered some values without submitting.
+closePwdModalBtn.addEventListener('click', () => {
+  changePwdForm.reset();
+});
+
 // Write code to run after an htmx request event.
-document.body.addEventListener('htmx:afterRequest', (e) => {
-  if (e.detail.requestConfig.path === 'v1/change-password' && e.detail.requestConfig.verb === 'post' && e.detail.successful) {
-    // Close the change password modal and reset the form.
-    const changePwdModal = document.getElementById('change_pwd_modal');
-    const changePwdForm = document.getElementById('change-pwd-form');
+  document.body.addEventListener('htmx:afterRequest', (e) => {
+    changePwdForm.reset();
+
+  if (e.detail.requestConfig.path === '/v1/change-password' && e.detail.requestConfig.verb === 'post' && e.detail.successful) {
+    // Close the modal and reset the form.
     changePwdModal.close();
     changePwdForm.reset();
 
     // Show success toast message
     const successToast = document.getElementById('success-toast');
-    const successToastMessage = document.getElementById('success-toast-message');
+    const successToastMessage = document.getElementById('success-toast-msg');
     successToast.classList.remove('hidden');
-    successToastMessage.textContent = 'Success! The password has been changed!';
+    successToastMessage.textContent = 'Success! The password has been changed.';
 
     setTimeout(() => {
       successToast.classList.add('hidden');
