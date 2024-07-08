dd58bbdb7c9cc331251b07f651e7c995239884d5
daniellemaxwell <danielle@rotational.io>
2024-07-08 15:07:17 -0400

    Allow admin users to reset any users password

--- pkg/web/static/js/userProfile.js
+++ pkg/web/static/js/userProfile.js
@@ -1,6 +1,8 @@
+import { setSuccessToast } from "./utils.js";
+
 // Helper function to add the same event handler for multiple events
-const addEventListeners = (el, evts, fn) => {
-  evts.split(' ').forEach(e => el.addEventListener(e, fn, false));
+export const addEventListeners = (el, evts, fn) => {
+  evts.split(' ').forEach(e => el?.addEventListener(e, fn, false));
 }
 
 // Elements referenced by this script
@@ -12,8 +14,8 @@ const confirmPassword = document.getElementById('confirm-password');
 const changePasswordBtn = document.getElementById('change-password-btn');
 
 // Reset the form when change password modal is closed in case user entered some values without submitting.
-closePwdModalBtn.addEventListener('click', () => {
-  changePwdForm.reset();
+closePwdModalBtn?.addEventListener('click', () => {
+  changePwdForm?.reset();
 });
 
 // Enable the change password button when the password and confirm password inputs are identical.
@@ -33,22 +35,13 @@ addEventListeners(confirmPassword, 'change keyup paste cut', checkPasswords);
 // Write code to run after an htmx request event.
 // TODO: should we do a pre-flight check as a last sanity check that the passwords match?
 document.body.addEventListener('htmx:afterRequest', (e) => {
-    changePwdForm.reset();
-    changePasswordBtn.setAttribute('disabled', 'true');
+    changePwdForm?.reset();
+    changePasswordBtn?.setAttribute('disabled', 'true');
 
   if (e.detail.requestConfig.path === '/v1/change-password' && e.detail.requestConfig.verb === 'post' && e.detail.successful) {
     // Close the modal and reset the form.
     changePwdModal.close();
     changePwdForm.reset();
-
-    // Show success toast message
-    const successToast = document.getElementById('success-toast');
-    const successToastMessage = document.getElementById('success-toast-msg');
-    successToast.classList.remove('hidden');
-    successToastMessage.textContent = 'Success! The password has been changed.';
-
-    setTimeout(() => {
-      successToast.classList.add('hidden');
-    }, 2250);
+    setSuccessToast('Success! The password has been changed.');
   };
 });
