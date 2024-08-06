744f945a761f95737370ece93b2fdf41a315d76b
Danielle <danielle@rotational.io>
2024-08-06 13:08:14 -0400

    Replace hyperscript in accounts template w/ JS function (#189)

--- pkg/web/static/js/networkSelect.js
+++ pkg/web/static/js/networkSelect.js
@@ -2,7 +2,7 @@ import { networksArray } from "./constants.js";
 
 const networkContentDiv = document.getElementById('network-content')
 // Display a searchable dropdown for networks.
-const networkSelect = new SlimSelect({
+export const networkSelect = new SlimSelect({
   select: '#networks',
   settings: {
     contentLocation: networkContentDiv,
