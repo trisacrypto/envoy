ed5a6dca441821b78612a16b8694b6e6a14c4681
daniellemaxwell <danielle@rotational.io>
2024-08-14 17:34:14 -0400

    Add copy to add key modal, check permissions type to determine if it needs to be set to an array

--- pkg/web/static/js/apikeys.js
+++ pkg/web/static/js/apikeys.js
@@ -3,12 +3,14 @@ import { setSuccessToast } from "./utils.js";
 
 const apiKeysEP = '/v1/apikeys';
 const addApiKeyModal = document.getElementById('add_apikey_modal');
+const closeApiKeyModal = document.getElementById('close-apikey-modal');
 const addApiKeyForm = document.getElementById('new-apikey-form');
+const customAccess = document.getElementById('custom-access');
 
 // Reset add api key modal form if user closes the modal.
-if (addApiKeyModal) {
-  addApiKeyModal.addEventListener('click', () => {
-    addApiKeyForm?.reset()
+if (closeApiKeyModal) {
+  closeApiKeyModal.addEventListener('click', () => {
+    addApiKeyForm?.reset();
   });
 };
 
@@ -17,26 +19,34 @@ document.body.addEventListener('htmx:configRequest', (e) => {
   // Set API key permissions if the user selects full access.
   if (e.detail.path === apiKeysEP && e.detail.verb === 'post') {
     const params = e.detail.parameters
-    const permissions = params.permissions === 'full' ? API_KEY_PERMISSIONS : params.permissions
-    params.permissions = permissions
+
+    // If full access is selected, send all permissions values to the BE.
+    if (params.permissions === 'full') {
+      params.permissions = API_KEY_PERMISSIONS || params.permissions
+    };
+
+    // If only 1 param is selected and its value isn't full, send it as an array.
+    if (params.permission !== 'full' && typeof(params.permissions) === 'string') {
+      params.permissions = [params.permissions]
+    };
   };
 });
 
-// Add code to run after an htmx request.
 document.body.addEventListener('htmx:afterRequest', (e) => {
-  // Close the add API key modal and reset the form after a successful request.
   if (e.detail.requestConfig.path === apiKeysEP && e.detail.requestConfig.verb === 'post' && e.detail.successful) {
-    addApiKeyModal.close()
-    addApiKeyModal.reset()
-    setSuccessToast('Success! The API key has been created.')
+    addApiKeyModal.close();
+    addApiKeyForm.reset();
+    // Scroll to the top of the custom access section in the modal.
+    customAccess.scrollTo(0, 0);
+    setSuccessToast('Success! The API key has been created.');
   };
-});
+})
 
 // Add code to run after htmx settles the DOM once a swap occurs.
 document.body.addEventListener('htmx:afterSettle', (e) => {
   // Add code to copy client secret and client ID to clipboard.
   if (e.detail.requestConfig.path === apiKeysEP && e.detail.requestConfig.verb === 'post') {
-    const copyIdBtn= document.getElementById('copy-id-btn');
+    const copyIdBtn = document.getElementById('copy-id-btn');
     const copySecretBtn = document.getElementById('copy-secret-btn');
 
     if (copyIdBtn) {
@@ -52,7 +62,7 @@ document.body.addEventListener('htmx:afterSettle', (e) => {
 // TODO: Create one function to copy client ID and client secret.
 function copyClientID() {
   const clientID = document.getElementById('client-id').textContent;
-  navigator.clipboard.writeText(clientID);
+  navigator.clipboard.writeText(`Client ID: ${clientID}`);
 
   const copyIdIcon = document.getElementById('copy-id-icon');
   copyIdIcon.classList.remove('fa-copy');
@@ -66,7 +76,7 @@ function copyClientID() {
 
 function copyClientSecret() {
   const clientSecret = document.getElementById('client-secret').textContent;
-  navigator.clipboard.writeText(clientSecret);
+  navigator.clipboard.writeText(`Client Secret: ${clientSecret}`);
 
   const copySecretIcon = document.getElementById('copy-secret-icon');
   copySecretIcon.classList.remove('fa-copy');
