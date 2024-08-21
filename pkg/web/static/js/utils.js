8944bde711bf56c34013f47fe77d9e6829f219e1
daniellemaxwell <danielle@rotational.io>
2024-08-21 09:45:20 -0400

    Update comment

--- pkg/web/static/js/utils.js
+++ pkg/web/static/js/utils.js
@@ -4,7 +4,7 @@ export function setSuccessToast(msg) {
   successToast.classList.remove('hidden');
   successToastMsg.textContent = msg;
 
-  // Remove the toast after 2.5 seconds.
+  // Remove the toast message.
   setTimeout(() => {
     successToast.classList.add('hidden');
   }, 2250);
