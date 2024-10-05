3354f8bf6ecd8d4a0a2f5a3d86ecf965dfc14bfe
Benjamin Bengfort <benjamin@bengfort.com>
2024-10-05 09:20:44 -0500

    version bump v0.27.0wq

--- pkg/version.go
+++ pkg/version.go
@@ -5,10 +5,10 @@ import "fmt"
 // Version component constants for the current build.
 const (
 	VersionMajor         = 0
-	VersionMinor         = 26
+	VersionMinor         = 27
 	VersionPatch         = 0
 	VersionReleaseLevel  = "beta"
-	VersionReleaseNumber = 31
+	VersionReleaseNumber = 32
 )
 
 // Set the GitVersion via -ldflags="-X 'github.com/trisacrypto/envoy/pkg.GitVersion=$(git rev-parse --short HEAD)'"
