658a88ea51ed18aac473cdb2b0b06ceb9490137f
Benjamin Bengfort <benjamin@bengfort.com>
2024-11-05 15:30:42 -0600

    fix sunrise typo

--- pkg/web/routes.go
+++ pkg/web/routes.go
@@ -117,7 +117,7 @@ func (s *Server) setupRoutes() (err error) {
 	{
 		sunrise.GET("/verify", s.VerifySunriseUser)
 		sunrise.GET("/accept", s.SunriseMessagePreview)
-		s.router.GET("/message", authenticate, s.SendMessageForm)
+		sunrise.GET("/message", authenticate, s.SendMessageForm)
 	}
 
 	// API Routes (Including Content Negotiated Partials)
