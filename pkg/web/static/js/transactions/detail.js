241989544e3af7c379708b8d198882df1feb26f3
Benjamin Bengfort <benjamin@rotational.io>
2025-03-16 16:02:19 -0500

    Transaction Reject (#289)

--- pkg/web/static/js/transactions/detail.js
+++ pkg/web/static/js/transactions/detail.js
@@ -1,10 +1,84 @@
 /*
+Application code for the transaction detail page.
+*/
+
+import { isRequestMatch } from '../htmx/helpers.js';
+import { createChoices } from '../modules/components.js';
+
+
+/*
+Specialized add alerts function for the transfer detail page.
+
+TODO: consider refactoring this into a more general alerts class.
+*/
+function alertError(id, title, message) {
+  const alerts = document.getElementById(id);
+  alerts.insertAdjacentHTML('beforeend', `
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+      <strong>${title}</strong> ${message}.
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+  `);
+}
+
+
+/*
+Pre-flight request configuration for htmx requests.
+*/
+document.body.addEventListener("htmx:configRequest", function(e) {
+  /*
+  Retry checkbox needs to be stored as a boolean value.
+  */
+ if (isRequestMatch(e, /\/v1\/transactions\/[A-Fa-f0-9-]{36}\/reject/, "post")) {
+    const params = new FormData();
+    e.detail.parameters.forEach((value, key) => {
+      if (key === "retry") {
+        value = value === "on" ? "true" : "false";
+        key = "json:retry";
+      }
+      params.append(key, value);
+    });
+
+    e.detail.parameters = params;
+  }
+});
+
+
+/*
 Post-event handling after htmx has settled the DOM.
 */
 document.addEventListener("htmx:afterSettle", function(e) {
-    // Initialize the status tooltips
-    const tooltips = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-    tooltips.forEach(tooltip => {
-      new Tooltip(tooltip);
-    });
+    if (isRequestMatch(e, /\/v1\/transactions\/[A-Fa-f0-9-]{36}/, "get")) {
+      // Initialize the status tooltips
+      const tooltips = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+      tooltips.forEach(tooltip => {
+        new Tooltip(tooltip);
+      });
+
+      const select = document.querySelector("select[name='code']");
+      createChoices(select);
+    }
+});
+
+/*
+Post-event handling when the transactions-updated event is fired.
+*/
+document.body.addEventListener("transactions-updated", function(e) {
+  const elt = e.detail?.elt;
+  if (elt) {
+    if (elt.id === 'rejectForm') {
+      const modal = Modal.getInstance(document.getElementById("rejectTransferModal"));
+      modal.hide();
+    }
+  }
+});
+
+/*
+Handle any htmx errors that are not swapped by the htmx config.
+*/
+document.body.addEventListener("htmx:responseError", function(e) {
+  if (isRequestMatch(e, /\/v1\/transactions\/[A-Fa-f0-9-]{36}\/reject/, "post")) {
+    const error = JSON.parse(e.detail.xhr.response);
+    alertError("rejectAlerts", "Error:", error.error);
+  }
 });
--- pkg/web/transactions.go
+++ pkg/web/transactions.go
@@ -748,7 +748,7 @@ func (s *Server) RejectTransaction(c *gin.Context) {
 	// respond with a 204 no content response and the front-end will handle the
 	// success message in the toast.
 	if c.NegotiateFormat(binding.MIMEJSON, binding.MIMEHTML) == binding.MIMEHTML {
-		htmx.Trigger(c, "transactionRejected")
+		htmx.Trigger(c, htmx.TransactionsUpdated)
 		return
 	}
 
