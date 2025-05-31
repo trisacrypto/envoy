e746cca8e591e5291c69ddeb9a3a570885ea23f5
Chris Okuda <chris@okuda.dev>
2025-05-31 13:06:38 -1000

    store test modifications

--- pkg/store/mock/store_test.go
+++ pkg/store/mock/store_test.go
@@ -9,58 +9,69 @@ import (
 	"github.com/trisacrypto/envoy/pkg/store/mock"
 )
 
-func TestOpenStore(t *testing.T) {
-	store, err := mock.Open(&dsn.DSN{Scheme: dsn.Mock})
-	require.NoError(t, err)
-	require.NotNil(t, store)
+//==========================================================================
+// Helpers
+//==========================================================================
+
+// Setup a mock store for tests.
+func setupStore() (store *mock.Store) {
+	var err error
+	if store, err = mock.Open(&dsn.DSN{Scheme: dsn.Mock}); err != nil {
+		panic("Error when setting up the mock store")
+	}
+	return store
 }
 
-func TestCloseStore(t *testing.T) {
+//==========================================================================
+// Tests
+//==========================================================================
+
+func TestStoreOpen(t *testing.T) {
+	store, err := mock.Open(&dsn.DSN{Scheme: dsn.Mock})
+	require.NoError(t, err, "error when opening store")
+	require.NotNil(t, store, "store was nil")
+}
+
+func TestStoreClose(t *testing.T) {
 
 	t.Run("Success", func(t *testing.T) {
 		// setup
-		store, err := mock.Open(&dsn.DSN{Scheme: dsn.Mock})
-		require.NoError(t, err)
-		require.NotNil(t, store)
+		store := setupStore()
 
 		// test
-		err = store.Close()
-		require.NoError(t, err)
+		err := store.Close()
+		require.NoError(t, err, "error when closing store")
 		store.AssertCalls(t, "Close", 1)
 	})
 
 	t.Run("CallbackWithError", func(t *testing.T) {
 		// setup
-		store, err := mock.Open(&dsn.DSN{Scheme: dsn.Mock})
-		require.NoError(t, err)
-		require.NotNil(t, store)
+		store := setupStore()
 		store.OnClose(func() error { return errors.ErrInternal }) // the error type doesn't matter
 
 		// test
-		err = store.Close()
-		require.Error(t, err, "expected an error calling Close()")
+		err := store.Close()
+		require.Error(t, err, "expected an error calling Close")
 		store.AssertCalls(t, "Close", 1)
 	})
 }
 
-func TestMultipleCallbacksWithReset(t *testing.T) {
+func TestStoreMultipleCallbacksWithReset(t *testing.T) {
 	// setup
-	store, err := mock.Open(&dsn.DSN{Scheme: dsn.Mock})
-	require.NoError(t, err)
-	require.NotNil(t, store)
+	store := setupStore()
 
 	// test
-	err = store.Close()
-	require.NoError(t, err, "there was an error calling Close()")
+	err := store.Close()
+	require.NoError(t, err, "there was an error calling Close")
 	store.AssertCalls(t, "Close", 1)
 
 	err = store.Close()
-	require.NoError(t, err, "there was an error calling Close()")
+	require.NoError(t, err, "there was an error calling Close")
 	store.AssertCalls(t, "Close", 2)
 
 	store.Reset()
 
 	err = store.Close()
-	require.NoError(t, err, "there was an error calling Close()")
+	require.NoError(t, err, "there was an error calling Close")
 	store.AssertCalls(t, "Close", 1)
 }
