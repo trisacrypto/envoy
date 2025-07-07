766fdcda068923326557c720210dc096d99924e2
Chris Okuda <chris@okuda.dev>
2025-07-07 13:54:27 -1000

    save stubs

---
+++ pkg/web/audit.go
@@ -0,0 +1,13 @@
+package web
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func (s *Server) ListComplianceAuditLogs(c *gin.Context) {
+	// FIXME: ListComplianceAuditLogs
+}
+
+func (s *Server) ComplianceAuditLogDetail(c *gin.Context) {
+	// FIXME: ComplianceAuditLogDetail
+}
---
+++ pkg/web/audit_test.go
@@ -0,0 +1,5 @@
+package web_test
+
+//FIXME: ListComplianceAuditLog tests (tests for all filter inputs and test the output possibilities)
+
+//FIXME: ComplianceAuditLogDetail tests (should just be simple get/check tests for a few cases)
